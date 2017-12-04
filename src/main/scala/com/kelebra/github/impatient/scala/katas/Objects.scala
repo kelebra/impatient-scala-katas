cbf95ca6c63dfbd678b3ad625c069075993e74ac
Oleksii Tkachuk <kelebra20@gmail.com>
2017-12-04 17:59:36 -0600

    Changes approach in chapter 6

--- src/main/scala/com/kelebra/github/impatient/scala/katas/Objects.scala
+++ src/main/scala/com/kelebra/github/impatient/scala/katas/Objects.scala
@@ -2,7 +2,7 @@ package com.kelebra.github.impatient.scala.katas
 
 import java.awt.Color
 
-import scala.language.implicitConversions
+import scala.language.{implicitConversions, postfixOps}
 
 /**
   * For given units provide convenient conversion and creation methods
@@ -14,48 +14,42 @@ case class Meters(value: BigDecimal) extends AnyVal
 
 case class Miles(value: BigDecimal) extends AnyVal
 
-object Inches {
+object LengthUnitsImplicits {
 
-  implicit def fromBigDecimalView[T](v: T)
-                                    (implicit cnv: T => BigDecimal): Inches =
-    Inches(cnv(v))
-}
+  implicit class ViewToLengthUnit[T](v: T)(implicit cnv: T => BigDecimal) {
 
-object Meters {
+    def inches = Inches(v)
 
-  implicit def fromBigDecimalView[T](v: T)
-                                    (implicit cnv: T => BigDecimal): Meters =
-    Meters(cnv(v))
-}
+    def meters = Meters(v)
 
-object Miles {
+    def miles = Miles(v)
+  }
 
-  implicit def fromBigDecimalView[T](v: T)
-                                    (implicit cnv: T => BigDecimal): Miles =
-    Miles(cnv(v))
 }
 
 object UnitConversion {
 
+  import LengthUnitsImplicits.ViewToLengthUnit
+
   implicit class InchesConversion(inches: Inches) {
 
-    def toMeters: Meters = inches.value * 0.0254
+    def toMeters: Meters = (inches.value * 0.0254) meters
 
-    def toMiles: Miles = inches.value * 1.578E-5
+    def toMiles: Miles = (inches.value * 1.578E-5) miles
   }
 
   implicit class MetersConversion(meters: Meters) {
 
-    def toInches: Inches = meters.value * 39.37007874
+    def toInches: Inches = (meters.value * 39.37007874) inches
 
-    def toMiles: Miles = meters.value * 0.00062137
+    def toMiles: Miles = (meters.value * 0.00062137) miles
   }
 
   implicit class MilesConversion(miles: Miles) {
 
-    def toInches: Inches = miles.value * 63360
+    def toInches: Inches = (miles.value * 63360) inches
 
-    def toMeters: Miles = miles.value * 1609.34709
+    def toMeters: Miles = (miles.value * 1609.34709) miles
   }
 
 }
--- src/test/scala/com/kelebra/github/impatient/scala/katas/ObjectsSpec.scala
+++ src/test/scala/com/kelebra/github/impatient/scala/katas/ObjectsSpec.scala
@@ -3,6 +3,7 @@ package com.kelebra.github.impatient.scala.katas
 import java.awt.Color
 
 import com.kelebra.github.impatient.scala.katas.CardSuit._
+import com.kelebra.github.impatient.scala.katas.LengthUnitsImplicits.ViewToLengthUnit
 import com.kelebra.github.impatient.scala.katas.UnitConversion._
 import org.scalatest.{Matchers, WordSpec}
 
@@ -13,54 +14,45 @@ class ObjectsSpec extends WordSpec with Matchers {
   "Inches" should {
 
     "be created with implicit conversion" in {
-      val inches: Inches = 100
-      inches.value shouldBe 100
+      (100 inches).value shouldBe 100
     }
 
     "convert to meters" in {
-      val inches: Inches = 100
-      (inches toMeters).value shouldBe 2.54
+      (100.inches toMeters).value shouldBe 2.54
     }
 
     "convert to miles" in {
-      val inches: Inches = 100
-      (inches toMiles).value shouldBe 0.001578
+      (100.inches toMiles).value shouldBe 0.001578
     }
   }
 
   "Meters" should {
 
     "be created with implicit conversion" in {
-      val meters: Meters = 100
-      meters.value shouldBe 100
+      (100 meters).value shouldBe 100
     }
 
     "convert to inches" in {
-      val meters: Meters = 100
-      (meters toInches).value shouldBe 3937.00787400
+      (100.meters toInches).value shouldBe 3937.00787400
     }
 
     "convert to miles" in {
-      val meters: Meters = 100
-      (meters toMiles).value shouldBe 0.06213700
+      (100.meters toMiles).value shouldBe 0.06213700
     }
   }
 
   "Miles" should {
 
     "be created with implicit conversion" in {
-      val miles: Miles = 100
-      miles.value shouldBe 100
+      (100 miles).value shouldBe 100
     }
 
     "convert to inches" in {
-      val miles: Miles = 100
-      (miles toInches).value shouldBe 6.336e+6
+      (100.miles toInches).value shouldBe 6.336e+6
     }
 
     "convert to meters" in {
-      val miles: Miles = 100
-      (miles toMeters).value shouldBe 160934.709
+      (100.miles toMeters).value shouldBe 160934.709
     }
   }
 
@@ -69,15 +61,11 @@ class ObjectsSpec extends WordSpec with Matchers {
     val Eps = 1e-3
 
     "be transitive for meter through miles" in {
-      val meters: Meters = 100
-
-      meters.toMiles.toMeters.value should be(meters.value +- Eps)
+      (100 meters).toMiles.toMeters.value should be((100 meters).value +- Eps)
     }
 
     "be transitive for inches through meters" in {
-      val inches: Inches = 100
-
-      inches.toMeters.toInches.value should be(inches.value +- Eps)
+      (100 inches).toMeters.toInches.value should be((100 inches).value +- Eps)
     }
   }
 
